bbd3b03f0f296bc182028cf46494cc9289066102
todopiola <benavidesgarciafranco@gmail.com>
2026-01-20 21:02:51 +0100

    Arquitectura del proyecto estable y primeros formatos de estilo

---
+++ view/contacto.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+    <?php include ("../templates/header.html") ?>
+
+    <main>
+
+        <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+            </div>
+        </div>
+        </div>
+
+    </main>
+
+    <?php include ("../templates/footer.html") ?>
+
+</body>
+</html>
